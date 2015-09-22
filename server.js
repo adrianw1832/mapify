5f3b7e7efc6846842823def7877867782a4233cc
Diego Regules <diegoregules@gmail.com>
2015-09-22 15:37:27 +0100

    Mongo DB stores Tweets

--- server.js
+++ server.js
@@ -15,11 +15,32 @@
 var express = require('express');
 var router = express.Router();
 
+//* MONGO DB Connection with mongoose * //
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  //DO ANYTHING HERE//
+});
+
+//*TWEET SCHEMA AND MODEL FOR MONGO *//
+var tweetSchema = mongoose.Schema({
+  tweetID:      Number,
+  coordinates:  Array,
+  text:         String
+});
+
+//*TO USE TWEETSCHEMA - WE NEED TO CONVERT IT INTO A MODEL*//
+var Tweet = mongoose.model('Tweet', tweetSchema);
+
 /*Get home page */
 router.get('/', function (req, res) {
   res.render('index');
 });
 
+// * TWIT - TWITTER STREAM * //
 var Twit = require('twit');
 
 var T = new Twit({
@@ -33,15 +54,28 @@ var T = new Twit({
 //   console.log(data.statuses.length);
 // });
 
-var world = ['-180', '-90', '180', '90'];
-var stream = T.stream('statuses/filter', {
-  locations: world,
-  language: 'en'
-});
-// var counter = 0;
-stream.on('tweet', function (tweet) {
-  if (tweet.geo !== null) {
-    // counter++;
-    console.log(tweet);
-  }
-});
+// var world = ['-180', '-90', '180', '90'];
+// var stream = T.stream('statuses/filter', {
+//   locations: world,
+//   language: 'en'
+// });
+// // var counter = 0;
+// stream.on('tweet', function (tweet) {
+//   if (tweet.geo !== null) {
+//     var newTweet = new Tweet({
+//       tweetID:      tweet.id,
+//       coordinates:  tweet.coordinates.coordinates,
+//       text:         tweet.text
+//     })
+//     newTweet.save(function(err, newTweet){
+//       if(err) return console.error(err);
+//     })
+//     // console.log(tweet.coordinates.coordinates);
+//   }
+// });
+
+//* Show saved items in MongoDB *//
+Tweet.find(function(err,tweets){
+  if(err) return console.error(err);
+  console.log(tweets);
+})
