0e355605c798f4c3d91d66c50faa584c365f2098
leon-wee <leonwee27@gmail.com>
2015-09-22 16:46:52 +0100

    Spiked drawing tweets on canvas

---
+++ public/js/map.js
@@ -0,0 +1,14 @@
+function Map(scalingFactor, mapContext) {
+  this.scalingFactor = scalingFactor;
+  this.mapContext = mapContext;
+};
+
+// Untested Unit Tests
+Map.prototype.plotCoords = function(longitude, latitude) {
+  var xCoord = (longitude + 180) * this.scalingFactor;
+  var yCoord = (90 - latitude) * this.scalingFactor;
+  this.mapContext.beginPath();
+  this.mapContext.arc(xCoord, yCoord, 2, 0, Math.PI * 2, true);
+  this.mapContext.fillStyle = "#000000"
+  this.mapContext.fill();
+};
--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-// var express = require('express');
-// var app = express();
-// var http = require('http').Server(app);
-//
-// app.use(express.static('public'));
-//
-// app.get('/', function(req, res){
-//   res.sendFile('/index.html');
-// });
-//
-// http.listen(3000, function(){
-//   console.log('listening on *:3000');
-// });
-
 var express = require('express');
+var app = express();
+var http = require('http').Server(app);
 var router = express.Router();
+
+app.use(express.static('public'));
 
 /*Get home page */
 router.get('/', function (req, res) {
@@ -29,19 +19,27 @@ var T = new Twit({
   access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-// T.get('search/tweets', { q: 'obama since:2015-09-15', count: 100 }, function(err, data, response) {
-//   console.log(data.statuses.length);
+// T.get('search/tweets', { q: 'obama since:2015-09-15', count: 10 }, function(err, data, response) {
+//   console.log(data);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 // });
 
-var world = ['-180', '-90', '180', '90'];
-var stream = T.stream('statuses/filter', {
-  locations: world,
-  language: 'en'
-});
-// var counter = 0;
-stream.on('tweet', function (tweet) {
-  if (tweet.geo !== null) {
-    // counter++;
-    console.log(tweet);
-  }
-});
+// var world = ['-180', '-90', '180', '90'];
+// var stream = T.stream('statuses/filter', {
+//   locations: world,
+//   language: 'en'
+// });
+// // var counter = 0;
+// stream.on('tweet', function (tweet) {
+//   if (tweet.geo !== null) {
+//     // counter++;
+//     console.log(tweet);
+//   }
+// });
+
+// Singapore
+//   geo: { type: 'Point', coordinates: [ 1.314682, 103.893371 ] },
+//  coordinates: { type: 'Point', coordinates: [ 103.893371, 1.314682 ] },
+
