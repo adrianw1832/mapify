9ae20dac31435d78393f1b9dcbd2eb399a0b7549
Diego Regules <diegoregules@gmail.com>
2015-09-23 17:03:07 +0100

    TweetsCoordinates semi-ready and modular

---
+++ public/js/tweetsCoordinates.js
@@ -0,0 +1,20 @@
+var TweetModel = require('./models/tweet.js');
+// var App = require('./app.js');
+
+var allCoordinates = [];
+
+function queryCoords() {
+  TweetModel.find( {}, { coordinates: 1, _id: 0 }, function(err, coords) {
+    if(err) return console.error(err);
+    appendCoords(coords);
+    //App FUNCTION THAT PLOTS into MAP GOES HERE THEN PASSESS IN `allCoordinates` as ARGUMENT
+  });
+}
+
+function appendCoords(coords){
+  for (var i = 0; i < coords.length; i++) {
+    allCoordinates.push(coords[i].coordinates);
+  }
+}
+
+module.exports = queryCoords;
--- server.js
+++ server.js
@@ -5,29 +5,21 @@ var router = express.Router();
 
 app.use(express.static('public'));
 var streamTweets = require('./public/js/tweetStream.js');
+var plotCoords = require('./public/js/tweetsCoordinates.js');
+
+
+//* RUN TO POPULATE MONGODB with TWEETS *//
+//****************
 // streamTweets();
+
+//* (RUN WHEN READY) TO PLOT COORDS INTO APP
+// plotCoords();
+
+
 
 /*Get home page */
 router.get('/', function (req, res) {
   res.render('index');
 });
-
-
-//*BIG-Coordinates Array*//
-// var allCoordinates = [];
-// Tweet.find( {}, { coordinates: 1, _id: 0 }, function(err, coords) {
-//   if(err) return console.error(err);
-//   appendCoords(coords);
-// });
-
-// function appendCoords(coords){
-//   for (var i = 0; i < coords.length; i++) {
-//     allCoordinates.push(coords[i].coordinates);
-//   }
-// }
-// setTimeout(function() {
-//   console.log(allCoordinates);
-// }, 1000);
-
 
 
