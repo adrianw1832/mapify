41ea10f389fa3aa86db1ca9d6dcd391d3eede927
Adrian Wong <Adrian@Adrians-MBP.home>
2015-09-23 18:48:55 +0100

    implemented colour generator

---
+++ spec/colourGeneratorSpec.js
@@ -0,0 +1,15 @@
+colourGenerator = require('../public/js/colourGenerator');
+
+describe("colourGenerator", function () {
+  it('returns green when the sentiment value is above 0', function() {
+    expect(colourGenerator.generate(2)).toEqual('#18bd4a');
+  });
+
+  it('returns yellow when the sentiment value is 0', function() {
+    expect(colourGenerator.generate(0)).toEqual('#e6cc68');
+  });
+
+  it('returns red when the sentiment value is below 0', function() {
+    expect(colourGenerator.generate(-2)).toEqual('#cd3f45');
+  });
+})
--- spec/specHelper.js
+++ spec/specHelper.js
@@ -1,12 +1,12 @@
 exports.mockTweet = {
   text: 'Some Random Tweet',
   coords: [-1, -103]
-}
+};
 
 exports.mockTweetWithPunctuations = {
   text: "~><?/.,:'|Nothing!@#$%^&*()_+  left",
   coords: [-1, -103]
-}
+};
 
 exports.mockVeryPositiveTweet = ['such', 'a', 'happy', 'and', 'fun', 'person'];
 
