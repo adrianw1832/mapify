037e7709ea3701c05e0705065a6f8ba4e0648458
Diego Regules <diegoregules@gmail.com>
2015-09-24 15:14:24 +0100

    added mongodb-uri dependency and herokuURI

--- model/tweet.js
+++ model/tweet.js
@@ -1,11 +1,23 @@
 //* MONGO DB Connection with mongoose * //
-export MONGOLAB_URI = "mongodb://heroku_j63z0ktq:211konangia06n9312mniu22bq@ds051933.mongolab.com:51933/heroku_j63z0ktq"
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mapifyTest');
+var uriUtil = require('mongodb-uri');
+var herokuURI = uriUtil.formatMongoose("mongodb://heroku_j63z0ktq:211konangia06n9312mniu22bq@ds051933.mongolab.com:51933/heroku_j63z0ktq");
 
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
+/*
+ * Mongoose by default sets the auto_reconnect option to true.
+ * We recommend setting socket options at both the server and replica set level.
+ * We recommend a 30 second connection timeout because it allows for
+ * plenty of time in most operating environments.
+ */
+
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
+
+mongoose.connect(herokuURI, options);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 // db.once('open', function (callback) {
 //   //DO ANYTHING HERE//
 // });
