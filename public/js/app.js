959111427d39c0470d6b816877524a78049d7a90
Adrian Wong <Adrian@Adrians-MacBook-Pro.local>
2015-09-29 14:35:44 +0100

    implemented zoom and drag features

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   var mapContext = mapCanvas.getContext('2d');
   var baseWidth = 360;
   var baseHeight = 180;
-  var scalingFactor = 4.5;
+  var scalingFactor = 4;
   var mapWidth = baseWidth * scalingFactor;
   var mapHeight = baseHeight * scalingFactor;
   var map = new Map(scalingFactor, mapContext);
@@ -65,32 +65,68 @@ $(document).ready(function() {
   drawMapBackground();
   $('.tweetMap').hide();
 
+  trackTransforms(mapContext);
 
-  $(window).bind('mousewheel', function(event) {
-    if (event.originalEvent.wheelDelta >= 0) {
-      scalingFactor -= 0.1;
-    }
-    else {
-      scalingFactor += 0.1;
-    }
-    drawMapBackground();
-    map.redrawTweets(scalingFactor);
-    event.preventDefault();
-  });
+	var lastX = mapCanvas.width / 2, lastY = mapCanvas.height / 2;
+	var dragStart, dragged;
 
-  // FOR TESTING PLOTTING TWEETS
-  // function testPlot() {
-  //   map.plotCoords(104, 1, "#FAFBFA");
-  // }
+	mapCanvas.addEventListener('mousedown',function(event){
+    document.body.style.userSelect = 'none';
+		lastX = event.offsetX || (event.pageX);
+		lastY = event.offsetY || (event.pageY);
+		dragStart = mapContext.transformedPoint(lastX,lastY);
+		dragged = false;
+	},false);
 
-  // $('.testButton').click(function() {
-  //   testPlot();
-  // });
+	mapCanvas.addEventListener('mousemove',function(event){
+		lastX = event.offsetX || (event.pageX);
+		lastY = event.offsetY || (event.pageY);
+		dragged = true;
+		if (dragStart){
+			var newPoint = mapContext.transformedPoint(lastX,lastY);
+			mapContext.translate(newPoint.x - dragStart.x, newPoint.y - dragStart.y);
+		  map.redrawTweets(mapWidth, mapHeight);
+		}
+	},false);
 
-  // FOR TESTING IMAGES GETTING BASE64
-  // setTimeout(function() {
-  //   var dataURL = mapCanvas.toDataURL();
-  //   console.log(dataURL);
-  // }, 1);
+	mapCanvas.addEventListener('mouseup',function(event){
+		dragStart = null;
+		if (!dragged) zoom(event.shiftKey ? -scaleFactor : scaleFactor );
+	},false);
 
+	var scaleFactor = 1.1;
+	var zoom = function(clicks){
+		var point = mapContext.transformedPoint(lastX,lastY);
+		mapContext.translate(point.x, point.y);
+		var factor = Math.pow(scaleFactor,clicks);
+		mapContext.scale(factor,factor);
+		mapContext.translate(-point.x, -point.y);
+		map.redrawTweets(mapWidth, mapHeight);
+	};
+
+	var handleScroll = function(event){
+		var delta = event.wheelDelta ? event.wheelDelta/40 : event.detail ? -event.detail : 0;
+		if (delta) zoom(delta);
+		return event.preventDefault() && false;
+	};
+	mapCanvas.addEventListener('DOMMouseScroll',handleScroll,false);
+	mapCanvas.addEventListener('mousewheel',handleScroll,false);
+
+	function trackTransforms(mapContext){
+		var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
+		var xform = svg.createSVGMatrix();
+		mapContext.getTransform = function(){ return xform; };
+
+		var translate = mapContext.translate;
+		mapContext.translate = function(dx,dy){
+			xform = xform.translate(dx,dy);
+			return translate.call(mapContext,dx,dy);
+		};
+
+		var point  = svg.createSVGPoint();
+		mapContext.transformedPoint = function(xCoord, yCoord){
+			point.x = xCoord; point.y = yCoord;
+			return point.matrixTransform(xform.inverse());
+		};
+	}
 });
--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,11 @@
 function Map(scalingFactor, mapContext) {
   this.scalingFactor = scalingFactor;
   this.mapContext = mapContext;
-  this.dataStorage = [];
+  this.tweetArray = [];
 }
 
 Map.prototype.plotTweet= function(tweet) {
-  this.dataStorage.push(tweet);
+  this.tweetArray.push(tweet);
   var xCoord = (tweet.coordinates[0]+ 180) * this.scalingFactor;
   var yCoord = (90 - tweet.coordinates[1]) * this.scalingFactor;
   this.mapContext.beginPath();
@@ -14,13 +14,14 @@ Map.prototype.plotTweet= function(tweet) {
   this.mapContext.fill();
 };
 
-Map.prototype.redrawTweets = function(newScale) {
-  for (var i = 0; i < this.dataStorage.length; i++) {
-    var xCoord = (this.dataStorage[i].coordinates[0]+ 180) * newScale;
-    var yCoord = (90 - this.dataStorage[i].coordinates[1]) * newScale;
+Map.prototype.redrawTweets = function(width, height) {
+  this.mapContext.clearRect(0, 0, width, height);
+  for (var i = 0; i < this.tweetArray.length; i++) {
+    var xCoord = (this.tweetArray[i].coordinates[0]+ 180) * this.scalingFactor;
+    var yCoord = (90 - this.tweetArray[i].coordinates[1]) * this.scalingFactor;
     this.mapContext.beginPath();
     this.mapContext.arc(xCoord, yCoord, 1.5, 0, Math.PI * 2, true);
-    this.mapContext.fillStyle = this.dataStorage[i].sentimentColour;
+    this.mapContext.fillStyle = this.tweetArray[i].sentimentColour;
     this.mapContext.fill();
   }
 };
